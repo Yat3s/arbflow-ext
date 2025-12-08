6221edb2b3ce0db6042de0e3ac8abb5b552834fd
Chris Ye <hawkoyates@gmail.com>
2025-12-08 15:50:25 +0800

    Add tailwindcss

--- src/devtools/index.tsx
+++ src/devtools/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import '../index.css'
 import { DevTools } from './DevTools'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
   <React.StrictMode>
--- src/newtab/index.tsx
+++ src/newtab/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import '../index.css'
 import { NewTab } from './NewTab'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
   <React.StrictMode>
